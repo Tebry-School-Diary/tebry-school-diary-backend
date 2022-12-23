36d8060312392bcda03c3c6e0fa1d0197ca9720a
yehor kochetov <yehor.kochetov@gmail.com>
2022-12-23 18:48:28 +0100

    add port to env file

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {databaseConnection} from "./Database/DatabaseConnection";
+import * as process from "process";
+require("dotenv").config();
 
 async function bootstrap() {
   await databaseConnection.initialize()
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
