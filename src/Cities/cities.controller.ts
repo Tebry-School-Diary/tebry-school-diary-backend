9d5c20835ee12294b9d161b8e894f00324f28436
yehor kochetov <yehor.kochetov@gmail.com>
2022-12-28 12:18:18 +0100

    modify routes to cities controller

--- src/Cities/cities.controller.ts
+++ src/Cities/cities.controller.ts
@@ -4,7 +4,7 @@ import {CityResponse} from "./Response/CityResponse";
 import {AddCityDTO} from "./DTO/AddCityDTO";
 import {RemoveCityDTO} from "./DTO/RemoveCityDTO";
 
-@Controller()
+@Controller("city")
 export class CitiesController{
      private readonly citiesService: CitiesService;
 
@@ -13,20 +13,20 @@ export class CitiesController{
      }
 
     @HttpCode(200)
-    @Get("/cities")
+    @Get()
     public async GetAllCities(): Promise<Array<CityResponse> | never> {
          return await this.citiesService.GetAllCities();
     }
 
     @HttpCode(201)
-    @Post("/city")
+    @Post()
     @UsePipes(ValidationPipe)
     public async AddCity(@Body() addCityDTO: AddCityDTO): Promise<boolean> {
         return await this.citiesService.AddCity(addCityDTO);
     }
 
     @HttpCode(200)
-    @Delete("/city")
+    @Delete()
     @UsePipes(ValidationPipe)
     public async RemoveCity(@Body() removeCityDTO: RemoveCityDTO): Promise<boolean> {
         return await this.citiesService.RemoveCity(removeCityDTO)
