786cdc88738f9129901ce5fcf9fefe44dc8d4f39
yehor kochetov <yehor.kochetov@gmail.com>
2022-12-29 19:43:33 +0100

    again fix building response model

--- src/Buildings/Response/BuildingResponse.ts
+++ src/Buildings/Response/BuildingResponse.ts
@@ -4,7 +4,7 @@ export class BuildingResponse {
     public readonly buildingAddress: string;
 
     constructor(buildingId: number, buildingPrefix: string, buildingAddress: string) {
-        this.obuildingId = obuildingId;
+        this.buildingId = buildingId;
         this.buildingPrefix = buildingPrefix;
         this.buildingAddress = buildingAddress;
     }
